772ec594c91ccd444eda2bf89bf56f46d4731520
LuisP09 <pachecocruzluis@gmail.com>
2023-01-30 13:38:07 -0600

    Cambios en inicio, nosotros y dashboard

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -59,8 +59,8 @@
                             class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
                             <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden"
                                 style="background-image: url('/img/Iniciar.jpg');
-              background-size: cover;">
-                                <span class="mask bg-gradient-primary opacity-6"></span>
+                                background-size: cover; background-position: center;">
+                                <span class="mask bg-gradient-indigo opacity-1"></span>
                                 <h4 class="mt-5 text-white   font-weight-bolder position-relative">"La mayoria de la gente observa lo que es, y no lo que puede llegar a ser"</h4>
                                 <p class="text-white position-relative">Enlaza-TEA</p>
                             </div>
--- resources/views/pages/inicio.blade.php
+++ resources/views/pages/inicio.blade.php
@@ -8,7 +8,7 @@
         </div>
     </div>
 </div>
-<main class="main-content  mt-0">
+<header class="main-content  mt-0">
     <div class="page-header align-items-start min-vh-10 pt-2 pb-6 m-3 border-radius-lg" style="background-image: url('/img/tea-2.jpeg'); background-position: center;">
         <span class="mask bg-gradient-dark opacity-6"></span>
         <div class="container pt-6 pb-5 m-auto">
@@ -20,9 +20,42 @@
             </div>
         </div>
     </div>
+</header>
+
+<main class="container">
+    <h3 class="text-center m-4">Nuestro Propósito</h3>
+    <div class="inicio mb-2">
+        <!-- <div class="inicio__img">
+            <img class="img-fluid" src="/img/bg-inicio-1.jpg" alt="">
+        </div> -->
+        <div class="inicio__texto">
+            <p>Nosotros buscamos crear una solución informática que permita reducir la ansiedad, frustración, estrés, miedo y confusión de los familiares de menores con TEA, por medio de capacitación gratuita, así como, enlazarlos con especialistas de la región, quienes, mediante el uso de la plataforma incrementarán el número de citas, ganancias, además, mejorarán su organización y seguimiento de pacientes.</p>
+        </div>
+    </div>
+    <h3 class="text-center m-4">¿Qué ofrecemos?</h3>
+    <h4 class=" mb-4">ENLAZA-TEA, ofrece a:</h4>
+    <div class="ofrecemos mb-4">
+        <div class="ofrecemos__texto">
+            <h4 class="text-center">Familiares TEA</h4>
+            <p>Capacitación accesible y gratuita, organizada por temas tea relevantes y pertinentes como: atención y comportamiento; Alimentación y nutrición y atención alternativa, entre otros, además el enlace con especialistas de la región socialmente comprometidos, que brindan una atención y seguimiento profesionales.</p>
+        </div>
+        <div class="ofrecemos__imagen">
+            <img class="img-fluid" src="/img/bg-inicio-4.jpeg" alt="">
+        </div>
+    </div>
+    <div class="ofrecemos mb-4">
+        <div class="ofrecemos__texto">
+            <h4 class="text-center">Especialistas TEA</h4>
+            <p>Incrementar hasta en un 30% el número de consultas realizadas y las ganancias generadas por este concepto, además, acceso total al módulo de organización de citas y seguimiento de pacientes.</p>
+        </div>
+        <div class="ofrecemos__imagen">
+            <img class="img-fluid" src="/img/bg-inicio-3.jpeg" alt="">
+        </div>
+    </div>
 </main>
 
-<div class="container m-auto d-flex  justify-content-center ">
+
+<!-- <div class="container m-auto d-flex  justify-content-center ">
     <div class="card border-dark mb-1" style="max-width: 950px; background:white;">
         <div class="row flex-row-reverse align-items-center g-0">
             <div class="col-md-4">
@@ -39,7 +72,7 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 
 @include('layouts.footers.auth.footer')
 @endsection
